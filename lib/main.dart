33721e0e47d63850bd033e966846e62af8f44927
hott3 <113865871+hott3@users.noreply.github.com>
2025-06-09 01:32:32 +0900

    main.dartのbuildメソッドを簡略化し、コードの可読性を向上

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'pages/home_page.dart';
 
 void main() {
@@ -9,21 +10,19 @@ class FlowerApp extends StatelessWidget {
   const FlowerApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'お花の見頃確認',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.pink,
-        scaffoldBackgroundColor: Colors.white,
-        cardTheme: const CardTheme(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(16)),
-          ),
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'お花の見頃確認',
+    theme: ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.pink,
+      scaffoldBackgroundColor: Colors.white,
+      cardTheme: const CardThemeData(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
         ),
       ),
-      home: const HomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+    ),
+    home: const HomePage(),
+    debugShowCheckedModeBanner: false,
+  );
 }
